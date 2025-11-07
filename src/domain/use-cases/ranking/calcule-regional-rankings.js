e34322428a7ecdc1d55adf90b0a4ffea3e5dda71
Sebastião de Freitas Emerick Dutra <sebahdutra@gmail.com>
2025-11-07 17:30:54 -0300

    fix: active airspace verification on rankings calculation

--- src/domain/use-cases/ranking/calcule-regional-rankings.js
+++ src/domain/use-cases/ranking/calcule-regional-rankings.js
@@ -116,7 +116,7 @@ function selectValidFlights (flightsOfPilot) {
   flightsOfPilot.sort((a,b) => b.olcScore - a.olcScore)
   const selectedFlights = []
   for (const flight of flightsOfPilot) {
-    // if (verifyInvalidAirspaceCheck(flight)) continue
+    if (verifyInvalidAirspaceCheck(flight)) continue
     selectedFlights.push(flight)
     if (selectedFlights.length === 6) break
   }
