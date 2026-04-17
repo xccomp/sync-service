d8832c7989323924001a521a03fe546df4dbb018
Sebastião de Freitas Emerick Dutra <sebahdutra@gmail.com>
2026-04-17 12:52:22 -0300

    feat: adiciona busca de cidades ibge de qualquer estados brasileiros

--- src/domain/use-cases/load-cities-from-ibge.js
+++ src/domain/use-cases/load-cities-from-ibge.js
@@ -1,43 +1,70 @@
 import XCCompDB from "#libs/xccomp-db/index.js"
 import axios from "axios"
 
-export async function loadCitiesFromIbge ()  {
-  const citiesData = await getCitiesFromIbgeApi()
+export async function loadCitiesFromIbge (uf)  {
+  const citiesData = await getCitiesFromIbgeApi(uf)
   await saveCities(citiesData)
   return true
 }
 
-async function getCitiesFromIbgeApi () {
-  const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/MG/municipios'
+async function getCitiesFromIbgeApi (uf) {
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
   const cities = await (await axios.get(url)).data
   return cities.map(city => ({
     id: city.id,
     name: city.nome,
     microregion: city.microrregiao.nome,
     mesoregion: city.microrregiao.mesorregiao.nome,
-    uf: city.microrregiao.mesorregiao.UF.nome,
-    region: city.microrregiao.mesorregiao.UF.regiao.nome
+    state: city.microrregiao.mesorregiao.UF.nome,
+    region: city.microrregiao.mesorregiao.UF.regiao.nome,
+    country: 'Brasil'
   }))
 }
 
 async function saveCities (citiesData) {
   let dbClient = null
   try {
-    dbClient = await XCCompDB.getClient()  
+    const values = {
+      ids: [],
+      names: [],
+      microregions: [],
+      mesoregions: [],
+      states: [],
+      regions: [],
+      countries: []
+    }
+    citiesData.forEach(city => {
+      values.ids.push(city.id)
+      values.names.push(city.name)
+      values.microregions.push(city.microregion)
+      values.mesoregions.push(city.mesoregion)
+      values.states.push(city.state)
+      values.regions.push(city.region)
+      values.countries.push(city.country)
+    })
     const sql = `
-      INSERT INTO
-        ibge_cities(id, name, microregion, mesoregion, uf, region)
-      VALUES  
-        ${citiesData.map(city => `(
-          ${city.id},
-          $$${city.name}$$,
-          $$${city.microregion}$$,
-          $$${city.mesoregion}$$,
-          $$${city.uf}$$,
-          $$${city.region}$$)
-        `).join(',')}
+      INSERT INTO ibge_cities (id, name, microregion, mesoregion, state, region, country)
+      SELECT * FROM UNNEST(
+        $1::int[], 
+        $2::text[], 
+        $3::text[], 
+        $4::text[], 
+        $5::text[], 
+        $6::text[], 
+        $7::text[]
+      )
+      ON CONFLICT (id) DO UPDATE SET
+        name = EXCLUDED.name,
+        microregion = EXCLUDED.microregion,
+        mesoregion = EXCLUDED.mesoregion,
+        state = EXCLUDED.state,
+        region = EXCLUDED.region,
+        country = EXCLUDED.country,
+        updated_at = NOW()
+      ;
     `
-    await dbClient.query(sql)  
+    dbClient = await XCCompDB.getClient()  
+    await dbClient.query(sql, Object.values(values))  
   } catch (error) {
     throw error
   } finally {
